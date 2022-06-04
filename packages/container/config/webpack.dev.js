8eceb0ddb5addb55af760056e4c06bf94bbb69ab
aymen kani <aymenkani554@gmail.com>
2022-06-04 15:59:21 +0100

    initial commit

---
+++ packages/container/config/webpack.dev.js
@@ -0,0 +1,29 @@
+const { merge }  = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const commonCOnfig = require('./webpack.common');
+const packageJson = require('../package.json');
+
+const devConfig = {
+    mode: 'development',
+    devServer: {
+        port: 8080,
+        historyApiFallback: {
+            index: 'index.html'
+        },
+        shared: packageJson.dependencies
+    },
+    plugins: [
+        new ModuleFederationPlugin({
+            name: 'container',
+            remotes: {
+                marketing: 'marketing@http://localhost:8081/remoteEntry.js',
+            }
+        }),
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+        })
+    ]
+}
+
+module.exports = merge(commonCOnfig, devConfig)
