17e67e01bdb8b3f11e1828455757ff77120954e9
aymen kani <aymenkani554@gmail.com>
2022-06-05 18:35:42 +0100

    added auth subapp

--- packages/auth/config/webpack.dev.js
+++ packages/auth/config/webpack.dev.js
@@ -1,29 +1,33 @@
 const { merge }  = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
-const commonCOnfig = require('./webpack.common');
+const commonConfig = require('./webpack.common');
 const packageJson = require('../package.json');
 
-// test 2
 const devConfig = {
     mode: 'development',
     output: {
         publicPath: 'http://localhost:8080/',
     },
     devServer: {
-        port: 8080,
+        port: 8082,
         historyApiFallback: {
             index: 'index.html'
-        },
+        }
     },
     plugins: [
         new ModuleFederationPlugin({
-            name: 'container',
-            remotes: {
-                marketing: 'marketing@http://localhost:8081/remoteEntry.js',
+            name: 'auth',
+            filename: 'remoteEntry.js',
+            exposes: {
+                './AuthApp': './src/bootstrap'
             },
-            shared: packageJson.dependencies
+            shared: packageJson.dependencies,
         }),
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+        })
     ]
 }
 
-module.exports = merge(commonCOnfig, devConfig)
+module.exports = merge(commonConfig, devConfig)
